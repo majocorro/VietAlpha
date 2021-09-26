5e7704247222cfc492b0cc196047da038d457f21
Marco Erik Gomez <marcoerik.gomez@gmail.com>
2021-09-26 02:05:53 -0400

    Add project files.

---
+++ VietAlpha/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using VietAlpha.Models;
+
+namespace VietAlpha.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Unit01()
+        {
+            return View();
+        }
+
+        public IActionResult Unit02()
+        {
+            return View();
+        }
+
+        public IActionResult Unit03()
+        {
+            return View();
+        }
+
+        public IActionResult Unit04()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
